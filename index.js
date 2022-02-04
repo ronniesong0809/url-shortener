e591d65afd8386917a23f834ff333e44a9a9f53f
Ronnie Song <ronniesong0809@gmail.com>
2022-02-04 15:25:03 -0800

    add body-parser and openapi middleware

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const expressOasGenerator = require('express-oas-generator')
+const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 dotenv.config()
 
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 3000
 
+// middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+expressOasGenerator.init(app, {})
+
+// endpoints
 app.get('/', (req, res) => {
   res.json({ foo: 'bar' })
 })
