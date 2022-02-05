5a77f46222dd6f5864aeff54a978dfeddb585105
Ronnie Song <ronniesong0809@gmail.com>
2022-02-05 15:05:20 -0800

    fix: no duplicate urls are allowed to be created

--- handler.js
+++ handler.js
@@ -25,31 +25,49 @@ const long2Short = async (req, res) => {
   }
 
   let val = req.body.url
-  let key = to62HEX(toHashCode(val, 0))
+  let key = await generateUniqueKey(val)
 
-  let i = 1,
-    shortExist,
-    stopped = false
+  let longExist = await longUrlExist(val)
+  if (longExist) {
+    res.status(200).json({
+      url: `${process.env.BASE_URL}/${key}`,
+      message: 'url already exists'
+    })
+    return
+  }
+
+  urlModel.create(
+    { shortUrl: key, longUrl: val, timestamp: new Date() },
+    function (err) {
+      if (err) {
+        res.status(500).json({ error: err })
+        return
+      }
+      res.status(200).json({ url: `${process.env.BASE_URL}/${key}` })
+    }
+  )
+}
+
+const generateUniqueKey = async (val) => {
+  let key
+  let i = 0
+  let stopped = false
   while (!stopped) {
     key = to62HEX(toHashCode(val, i++))
-    shortExist = await shortUrlExist(key)
+    let shortExist = await shortUrlExist(key)
     if (!shortExist) {
       stopped = true
     }
   }
-
-  let url = new urlModel({ shortUrl: key, longUrl: val })
-  url.save(function(err) {
-    if (err) {
-      res.status(500).json({ error: err })
-      return
-    }
-    res.status(200).json({ url: `${process.env.BASE_URL}/${key}` })
-  })
+  return key
 }
 
 const shortUrlExist = (key) => {
   return urlModel.exists({ shortUrl: key })
+}
+
+const longUrlExist = (val) => {
+  return urlModel.exists({ longUrl: val })
 }
 
 module.exports = { short2Long, long2Short }
