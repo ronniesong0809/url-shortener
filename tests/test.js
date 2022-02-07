982b338998da7a0a7e1b56fbfc2ab716f1c557dc
Ronnie Song <ronniesong0809@gmail.com>
2022-02-06 18:26:14 -0800

    feat: add few more tests

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,14 @@
 const request = require('supertest')
 const app = require('../index')
 
+describe('Testing the post /shorten API without request body', () => {
+  it('POST /shorten', async () => {
+    const response = await request(app)
+      .post('/shorten')
+    expect(response.status).toBe(422)
+  })
+})
+
 describe('Testing the post /shorten API with request body', () => {
   it('POST /shorten', async () => {
     const response = await request(app)
@@ -10,20 +18,42 @@ describe('Testing the post /shorten API with request body', () => {
   })
 })
 
-describe('Testing the get /{url} API with the short url', () => {
+describe('Testing the post /shorten API once again', () => {
+  it('POST /shorten', async () => {
+    const response = await request(app)
+      .post('/shorten')
+      .send({ url: 'https://github.com/ronniesong0809/url-shortener' })
+    expect(response.status).toBe(200)
+  })
+})
+
+describe('Testing the get /{url} API with the incorrect short url', () => {
+  it('GET /{url}', async () => {
+    const response = await request(app).get('/123456')
+    expect(response.status).toBe(404)
+  })
+})
+
+describe('Testing the get /{url} API with the correct short url', () => {
   it('GET /{url}', async () => {
     const response = await request(app).get('/2h2mYC')
     expect(response.status).toBe(302)
   })
 })
 
-describe('Testing the delete /{url} API with the short url', () => {
+describe('Testing the delete /{url} API with the incorrect short url', () => {
+  it('DELETE /{url}', async () => {
+    const response = await request(app).delete('/123456')
+    expect(response.status).toBe(404)
+  })
+})
+
+describe('Testing the delete /{url} API with the correct short url', () => {
   it('DELETE /{url}', async () => {
     const response = await request(app).delete('/2h2mYC')
     expect(response.status).toBe(200)
   })
 })
-
 
 describe('Testing the get /all API to display all urls', () => {
   it('POST /all', async () => {
