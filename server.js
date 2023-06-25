b0dfb8d83a0c77f6a3399fd91c61d328eb91702b
Ronnie Song <ronniesong0809@gmail.com>
2023-06-25 14:05:10 -0700

    chore: removed express-oas-generator

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const expressOasGenerator = require('express-oas-generator')
-const { SPEC_OUTPUT_FILE_BEHAVIOR } = expressOasGenerator
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const urlController = require('./controller/urlController')
@@ -12,13 +10,6 @@ const mongoose = require('mongoose')
 require('./utils/database')
 
 // middleware
-expressOasGenerator.handleResponses(app, {
-  mongooseModels: mongoose.modelNames(),
-  specOutputPath: './api_docs.json',
-  specOutputFileBehavior: SPEC_OUTPUT_FILE_BEHAVIOR.PRESERVE,
-  alwaysServeDocs: true,
-  swaggerUiServePath: 'api/docs'
-})
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -31,6 +22,5 @@ app.get('/:url/stats', counterController.getUrlStats)
 app.post('/shorten', urlController.long2Short)
 app.put('/:url', urlController.extendExpiration)
 app.delete('/:url', urlController.deleteRecord)
-expressOasGenerator.handleRequests()
 
 module.exports = app
