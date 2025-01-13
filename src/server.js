30283db3f9444585f82c0810a17742a0fb1c0651
Ronnie Song <ronniesong0809@gmail.com>
2025-01-13 02:04:59 -0800

    chore: update dependencies

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const shortUrlRoutes = require('./routes/shortUrlRoutes')
@@ -11,8 +10,8 @@ dotenv.config()
 require('./config/database')
 
 // middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 // routes
